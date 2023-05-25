ece0562fa11df0d3df7d8ae0926cc4087a75a93b
SupremeMhr <126633255+SupremeMhr@users.noreply.github.com>
2023-05-25 11:21:39 +0545

    pizza shop.py

--- pizza shop.py
+++ pizza shop.py
@@ -1,3 +1,27 @@
+import csv
+import random
+
+def select_items(items, item_type):
+    selected_items = []
+
+    print(f"{item_type.capitalize()}s available:")
+    for item, price in items.items():
+        print(f"{item} - Rs{price}")
+
+    while True:
+        choice = input(f"Select a pizza or 'No' to proceed to checkout: ")
+
+        if choice.upper() == "NO":
+            break
+
+        if choice in items:
+            selected_items.append(choice)
+
+    return selected_items
+
+name = input("Enter your name: ")
+order_id = random.randint(0, 99999)
+
 pizza_shop = {
     "cheese": 500,
     "veg": 600,
@@ -10,38 +34,11 @@ topping_shop = {
     "mushroom": 150
 }
 
-# Empty lists created for storing selected pizza and toppings
-pizza_selected = []
-topping_selected = []
+# Select pizzas
+pizza_selected = select_items(pizza_shop, "pizza")
 
-print("Welcome to our Pizza Shop")
-print("Pizzas Available in our shop:")
-
-for pizza, price in pizza_shop.items():
-    print(f"{pizza} - Rs{price}")
-
-while True:
-    choice = input("Select a pizza or 'No' to proceed to checkout: ")
-
-    if choice.upper() == "NO":
-        break
-
-    if choice in pizza_shop:
-        pizza_selected.append(choice)
-
-print("Toppings available:")
-for topping, price in topping_shop.items():
-    print(f"{topping} - Rs{price}")
-
-topping_choice = input("Do you want to add toppings? (Yes/No): ")
-
-if topping_choice.lower() == "yes":
-    while True:
-        topping = input("Select a topping or 'No' to proceed: ")
-        if topping.upper() == "NO":
-            break
-        if topping in topping_shop:
-            topping_selected.append(topping)
+# Select toppings
+topping_selected = select_items(topping_shop, "topping")
 
 total = 0
 for pizza in pizza_selected:
@@ -50,3 +47,12 @@ for pizza in pizza_selected:
     total += subtotal
 
 print("Total: Rs" + str(total))
+
+# Writing order details to a CSV file
+with open("order_details.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Name", "Pizza", "Toppings", "Order ID", "Total"])
+    writer.writerow([name, ", ".join(pizza_selected), ", ".join(topping_selected), order_id, total])
+
+print("Thank you for your order,Pls visit again")
+print("Order details saved to order_details.csv")
